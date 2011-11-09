f7762dc7b5a5ea54d8baa747086433253c62af15
Pavithra Acharya <pavitra@opentracker.net>
2011-11-09 19:13:33 +0530

    fixed comments and documentation

--- OTLibrary/OTSend.h
+++ OTLibrary/OTSend.h
@@ -1,9 +1,9 @@
 //
-//  OTSend.h
-//  opentracker
+//    OTSend.h
+//    opentracker
 //
-//  Created by Pavitra on 9/29/11.
-//  Copyright 2011 Opentracker. All rights reserved.
+//    Created by Pavitra on 9/29/11.
+//    Copyright 2011 Opentracker. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -13,25 +13,27 @@
 /*!
  * @method urlEncoded
  * @abstract This method url encodes a given string
- * @param url The url paramter string to be encoded.
+ * @param url The url parameter string to be encoded.
+ * @return The Encoded url as string 
  */
 +(NSString *) urlEncoded : (NSString*) url;
 /*!
  * @method send
  * @abstract Sends the key value pairs to Opentracker's logging/ analytics engines via
- * HTTP POST requests.
- * Based on sending key value pairs documentated at:
- * http://api.opentracker.net/api/inserts/insert_event.jsp
+ *			 HTTP POST requests.
+ *			 Based on sending key value pairs documented at:
+ *			 http://api.opentracker.net/api/inserts/insert_event.jsp
  * @param keyValuePairs the key value pairs 
- *(plain text utf-8 strings) to send to the logging service.
+ *		    (plain text utf-8 strings) to send to the logging service.
  * @return a response string generated in opentracker.net
- * or nil if an exception is caught
+ *		     or nil if an exception is caught
  */
 +(NSString*) send: (NSMutableDictionary*) keyValuePairs;
 
 /*!
  * @method sendUrl
  * @abstract Sends a post request with the url via HTTP POST requests.
+ * @param url The url parameter string to be sent.
  * @return the response as string, null if an exception is caught
  */
 +(NSString*) sendUrl: (NSString*) url;
@@ -44,7 +46,13 @@
  * @return response string on upload.
  */
 +(BOOL) uploadFile:(NSString*) fileToSend toServer:(NSString*) uploadServer;
-
-+(NSString *)UUID;
+/*!
+ * @method UUID 
+ * @abstract Use to get the current device UUID - Universal Unique Identifier,
+ *                     Based on Apple's latest guidelines 2011.
+ *                     see discussion: 
+ * @return The UUID as a string 
+ */
++(NSString *)UUID;    
 
 @end
